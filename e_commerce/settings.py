605ff794445b314c64d1271855519b5ae4053bb9
challaharijoshika <challaharijoshika@gmail.com>
2025-09-27 19:29:04 +0530

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -149,6 +149,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# Optional: enable gzip and caching
+# Optional: enable gzip and caching
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
