95c3f5e5192fbdfd5eb8a954b9a568959ed35409
challaharijoshika <challaharijoshika@gmail.com>
2025-09-27 09:18:56 +0530

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -98,15 +98,20 @@ LOGOUT_REDIRECT_URL = 'login'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'e_commerce',
-        'USER': 'root', 
-        'PASSWORD': 'Hari@1925',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'e_commerce',
+    #     'USER': 'root', 
+    #     'PASSWORD': 'Hari@1925',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
