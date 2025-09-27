81a5d65638f8fd249e281a6e12357b1280336b80
challaharijoshika <challaharijoshika@gmail.com>
2025-09-27 09:27:57 +0530

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -83,9 +83,9 @@ REST_FRAMEWORK = {
 }
 WSGI_APPLICATION = "e_commerce.wsgi.application"
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-   "http://localhost:5173",   # React default port
-]
+# CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:5173",   # React default port
+# ]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
